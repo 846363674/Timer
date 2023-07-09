f438715110e5fad75b93ff2621da233c64175433
846363674 <846363674@qq.com>
2023-07-09 23:50:12 +0800

    调整页面显示，增加置顶功能

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import tkinter as tk
 import time
 
 app = tk.Tk()
+app.wm_attributes('-topmost',1)
 
-second = 5
+TOTAL_TIME = 5
+second = TOTAL_TIME
 is_running = False
-
 
 def time_to_string(s):
     """
@@ -30,6 +31,15 @@ def click_start_button():
         update_time()
 
 
+def click_reset_button():
+    global is_running, second
+    if not is_running:
+        start_button.config(text="开始")
+        is_running = False
+        second = TOTAL_TIME
+        dstr.set(time_to_string(second))
+
+
 def update_time():
     """
     更新时间
@@ -39,25 +49,29 @@ def update_time():
     # 倒计时完成停止运行
     if second <= 0:
         is_running = False
-        time_label.config(text=time_to_string(0))
-        start_button.config(text="停止")
+        dstr.set(time_to_string(0))
+        start_button.config(text="开始")
     # 每秒倒计时并刷新时间
     if is_running:
         second -= 1
-        time_label.config(text=time_to_string(second))
+        dstr.set(time_to_string(second))
         app.after(1000, update_time)
-
 
 menubar = tk.Menu(app)
 menubar.add_command(label='设置', command=None)
 app.config(menu=menubar)
 
-time_label = tk.Label(app, text=time_to_string(second), font=('Arial', 30))
-time_label.pack(pady=20)
+dstr = tk.StringVar()
+dstr.set(time_to_string(second))
+time_label = tk.Label(app, textvariable=dstr, font=('Arial', 30))
+time_label.grid(row=0, column=0, pady=10, columnspan=2)
 
-start_button = tk.Button(app, text="开始", font=('Arial', 18), width=10, height=3)
-start_button.pack(pady=10)
+start_button = tk.Button(app, text="开始", font=('Arial', 18), width=10, height=1)
+start_button.grid(row=1, column=0, padx=10, pady=10)
 start_button.config(command=click_start_button)
 
-app.geometry("400x200")
+reset_button = tk.Button(app, text="重置", font=('Arial', 18), width=10, height=1)
+reset_button.grid(row=1, column=1, padx=10)
+reset_button.config(command=click_reset_button)
+
 app.mainloop()
